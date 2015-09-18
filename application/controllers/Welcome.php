8dba981009ff791910636937e87d4195fa8f7977
Georgi Hristov <hristov798@gmail.com>
2015-09-17 17:40:24 -0700

    Transformed webapp to use a database to lookup image filenames.
    Signed-off-by: Georgi Hristov <hristov798@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -41,6 +41,23 @@ class Welcome extends Application {
 
     public function index()
     {
+        // get 3 newest images
+        $pix = $this->images->newest();
+        
+        // build an array of formatted cells for them
+        foreach ($pix as $picture)
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        
+        // prime the table class
+        $this->load->library('table');
+        $params = array( 'table_open' => '<table class="gallery">'
+                       , 'cell_start' => '<td class="oneimage">'
+                       , 'cell_alt_start' => '<td class="oneimage">' );
+        $this->table->set_template($params);
+        
+        // generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
         //$this->load->view('welcome');
         $this->data['pagebody'] = 'welcome';
         $this->render();
